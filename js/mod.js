2cb31fbd440b4e1f726d834bac0c80d9e000f907
asdfg-langsd <84178005+asdfg-langsd@users.noreply.github.com>
2021-09-20 18:18:55 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,19 +1,19 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The PoinTree",
+	id: "pointree",
+	author: "gghu",
 	pointsName: "points",
 	discordName: "",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	
-	offlineLimit: 1,  // In hours
+	offlineLimit: 10000000000 ,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.0.1",
+	name: "Release beta",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
