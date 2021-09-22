cc79012915e28c1406eaf41acce0db97c074585c
asdfg-langsd <84178005+asdfg-langsd@users.noreply.github.com>
2021-09-22 13:03:39 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -37,35 +37,12 @@ addLayer("u", {
 	    cost: new Decimal(11),
 	    unlocked(){
                 return hasUpgrade("u", 11)
-	    }
-    }
-})
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#4BDC13",
-    requires: new Decimal(22), // Can be a function that takes requirement increases into account
-    resource: "upgrade points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(2)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(2)
-    },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true},
+	    },
+	21:{
+	    title:"doubles",
+	    description: "*2 base point gain",
+	    cost: new Decimal(50),
+	    
+	    
     }
 })
