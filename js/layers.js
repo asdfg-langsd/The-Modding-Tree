4cf510dff165eeae63415d563af0e4b5ffe5e7a1
asdfg-langsd <84178005+asdfg-langsd@users.noreply.github.com>
2021-10-08 15:41:15 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -79,7 +79,7 @@ addLayer("p", {
     color: "#4BDC13",
     requires: new Decimal(111), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
-    baseResource: "prrestige points", // Name of resource prestige is based on
+    baseResource: "upgrade points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
