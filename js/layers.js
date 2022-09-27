6b1a006f80e0d2b8bfdd943556de3ac5fc842d7b
asdfg-langsd <84178005+asdfg-langsd@users.noreply.github.com>
2022-09-27 21:37:51 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -71,7 +71,7 @@ addLayer("u", {
 addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 	points: new Decimal(0)
@@ -90,7 +90,7 @@ addLayer("p", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: reset Prestige Points", onPress(){if (canReset(this.layer)) doReset(this.layer)}}
     ],
